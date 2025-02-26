3d44b4ae3c206cfbd73aa88fa339bcc5345d59ff
Daisel10 <102265953+daisel10@users.noreply.github.com>
2025-02-26 17:21:46 -0500

    feat(sfp_np): corregir de sfp_np

--- src/ur_os/SJF_P.java
+++ src/ur_os/SJF_P.java
@@ -8,40 +8,41 @@ package ur_os;
  *
  * @author prestamour
  */
-public class SJF_P extends Scheduler{
+public class SJF_P extends Scheduler {
 
     // DAVID NECESITAMOS IMPLEMENTAR EN AMBOS UN VERIFICADOR DE QUE LA CPU ESTÁ LLENA XD
-    SJF_P(OS os){
+    SJF_P(OS os) {
         super(os);
     }
-    
+
     @Override
-    public void newProcess(boolean cpuEmpty){// When a NEW process enters the queue, process in CPU, if any, is extracted to compete with the rest
-        if (!cpuEmpty && os.getProcessInCPU() != null)    {
+    public void newProcess(boolean cpuEmpty) {// When a NEW process enters the queue, process in CPU, if any, is extracted to compete with the rest
+        if (!cpuEmpty && os.getProcessInCPU() != null) {
             System.out.println("********** NEWPROCESS_SEVAD  ***********");
-        os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, os.getProcessInCPU());} // Así es que se interrumpe. Cuando haya un nuevo proceso implemente paramos, extraemos el proceso de la cpu.
-       getNext(false);} // Luego forzamos la evaluación.
+            os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, null);
+        } // Así es que se interrumpe. Cuando haya un nuevo proceso implemente paramos, extraemos el proceso de la cpu.
 
+    } // Luego forzamos la evaluación.
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty){// When a process return from IO and enters the queue, process in CPU, if any, is extracted to compete with the rest                
-        if (!cpuEmpty && os.getProcessInCPU() != null)    {
+    public void IOReturningProcess(boolean cpuEmpty) {// When a process return from IO and enters the queue, process in CPU, if any, is extracted to compete with the rest                
+        if (!cpuEmpty && os.getProcessInCPU() != null) {
             System.out.println("********** IORETURNING_SEVAD  ***********");
-            os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, os.getProcessInCPU()); // Así es que se interrumpe. Cuando haya un nuevo proceso implemente paramos, extraemos el proceso de la cpu.
-            getNext(false);}} // Luego forzamos la evaluación.
+            os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, null); // Así es que se interrumpe. Cuando haya un nuevo proceso implemente paramos, extraemos el proceso de la cpu.
+            // getNext(true);
+        }
+    } // Luego forzamos la evaluación.
 
-    
-   
     @Override
     public void getNext(boolean cpuEmpty) {
-        if (!processes.isEmpty() && cpuEmpty){
+        if (!processes.isEmpty() && cpuEmpty) {
             Process min_BTR_process = null;
             int min_BTR = Integer.MAX_VALUE;
 
-            for (Process p: processes){
-                if (p.isCurrentBurstCPU()){
+            for (Process p : processes) {
+                if (p.isCurrentBurstCPU()) {
                     int temp = p.getRemainingTimeInCurrentBurst();
-                    if(temp < min_BTR) {
+                    if (temp < min_BTR) {
                         min_BTR = temp;
                         min_BTR_process = p;
                     } else if (temp == min_BTR) {
@@ -50,8 +51,10 @@ public class SJF_P extends Scheduler{
                     }
                 }
             }
-        if (min_BTR_process != null){
-            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, min_BTR_process);
-            processes.remove(min_BTR_process);
-        }}    
-    }}
+            if (min_BTR_process != null) {
+                os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, min_BTR_process);
+                processes.remove(min_BTR_process);
+            }
+        }
+    }
+}
